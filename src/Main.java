105cbcd37f1d9369bc92de3f0971faf2cbf57f90
GustavoDLRA <A01410922@itesm.mx>
2018-05-14 16:54:30 -0500

    Ex 2.0

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         //Comenzamos por crear los objetos de tipo Artículo.
-        Articulo c1 = new Articulo("Manzana","Fruto rojo, amarillo o verde con semillas","$25 k","18/05/2018");
-        Articulo c2 = new Articulo("Leche","Bebida pasteurizada que viene de las vacas","$20 el litro","23/05/2018");
-        Articulo c3 = new Articulo("Pasta","Especial para hacer Lasagna","$10 el paquete","20/01/2019");
+        Articulo c1 = new Articulo("Manzana","Fruto rojo, amarillo o verde con semillas","$25 k","18/05/2018",10);
+        Articulo c2 = new Articulo("Leche","Bebida pasteurizada que viene de las vacas","$20 el litro","23/05/2018",20);
+        Articulo c3 = new Articulo("Pasta","Especial para hacer Lasagna","$10 el paquete","20/01/2019",10);
         //Ahora Adaptamos estos objetos, para ingresarlos dentro de un objeto generico de tipo Elemento<T>
         Elemento<Articulo> primero = new Elemento<>(c1);
         Elemento<Articulo> segundo = new Elemento<>(c2);
